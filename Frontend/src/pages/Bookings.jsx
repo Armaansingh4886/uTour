b9ae5f15a190d9c122864bfb34a1664e44a1099b
Armaansingh4886 <armaansingh4886@gmail.com>
2024-02-24 22:30:39 +0530

    bookings

--- Frontend/src/pages/Bookings.jsx
+++ Frontend/src/pages/Bookings.jsx
@@ -15,6 +15,20 @@ const Bookings = () => {
     <>
       <CommonSection title={"Bookings"} />
       {/* {bookings?.map((book)=>{<div>{book}</div>})}; */}
+      <Container>
+        <Row>{
+          data.length === 0 ? (
+<h4 className="text-center">No tour found</h4>
+          )  :(
+            data?.map(tour => (
+              <Col lg="3" className="mb-4" key={tour._id}>
+                <TourCard tour={tour} />
+              </Col>
+            ))
+          )
+           }
+        </Row>
+      </Container>
     </>
   );
 };
