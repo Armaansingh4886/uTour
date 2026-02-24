f0d2d28075047e86fd25a4add21c68092ab72456
Armaan Singh <89766696+Armaansingh4886@users.noreply.github.com>
2026-02-24 02:53:33 +0000

    refinement

--- Backend/index.js
+++ Backend/index.js
@@ -21,7 +21,7 @@ const corsOptions = {
 mongoose.set("strictQuery", false);
 const connect = async()=>{
     try {
-        await mongoose.connect('mongodb://localhost:27017',{
+        await mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser:true,
             useUnifiedTopology:true
         })
